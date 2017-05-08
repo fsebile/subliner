5c21f51f5542f6b7a103545d80ae44116bf2a2de
Feruz Davletov <fdavletov@gmail.com>
2017-05-08 15:58:10 +0300

    code

---
+++ src/main/java/org/sebo/Controller.java
@@ -0,0 +1,24 @@
+package org.sebo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
+
+public class Controller {
+
+    @FXML
+    public TextArea originalText;
+
+    @FXML
+    public TextArea translateText;
+
+    @FXML
+    public TableView table;
+
+    public void handleClose(ActionEvent event) {
+//        System.out.println(event);
+        System.exit(0);
+    }
+}
---
+++ src/main/java/org/sebo/Main.java
@@ -0,0 +1,28 @@
+package org.sebo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL view = getClass().getClassLoader().getResource("view.fxml");
+        System.out.println(view);
+
+//        assert view != null;
+        Parent root = FXMLLoader.load(view);
+        primaryStage.setTitle("Subliner");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
